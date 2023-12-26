7fb0fc15f6cdd13c5bbcc71b734e0097917ce67c
msisaif <msi132043@gmail.com>
2023-12-26 08:21:22 +0600

    Accessor & Mutator for domain column in client model

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,5 +16,15 @@ class Client extends Model
     public function scopeActive(Builder $query): void
     {
         $query->where('is_active', 1);
+    }
+
+    protected function domain(): Attribute
+    {
+        $pattern = '/^http(|s)\:\/\/(www\.|)|www./';
+
+        return Attribute::make(
+            get: fn (string $value) => rtrim(preg_replace($pattern, '', $value), '/'),
+            set: fn (string $value) => rtrim(preg_replace($pattern, '', $value), '/'),
+        );
     }
 }
