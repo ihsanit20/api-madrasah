06800b0509fb2a14dfbd5dcedbb32e61d47b3e53
msisaif <msi132043@gmail.com>
2024-04-20 00:48:38 +0600

    Resource data type custing in AcademicSubjectResource

--- app/Http/Resources/AcademicSubjectResource.php
+++ app/Http/Resources/AcademicSubjectResource.php
@@ -14,6 +14,19 @@ class AcademicSubjectResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            "id"        => (int) ($this->id ?? 0),
+            "priority"  => (int) ($this->priority ?? 0),
+            "is_active" => (boolean) ($this->is_active ?? false),
+
+            "author_id" => (int) ($this->author_id ?? 0),
+            "author"    => $this->whenLoaded('author'),
+            
+            "academic_class_id" => (int) ($this->academic_class_id ?? 0),
+            "academic_class"    => $this->whenLoaded('academic_class'),
+            
+            "department_class_subject_id"   => (int) ($this->department_class_subject_id ?? 0),
+            "department_class_subject"      => $this->whenLoaded('department_class_subject'),
+        ];
     }
 }
