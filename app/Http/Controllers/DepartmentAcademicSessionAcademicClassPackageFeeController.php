3a23e640b67ac76acbce0fc0a3c10a5b7a83e585
msisaif <msi132043@gmail.com>
2024-04-21 09:09:44 +0600

    Resource and Collection of PackageFee

--- app/Http/Controllers/DepartmentAcademicSessionAcademicClassPackageFeeController.php
+++ app/Http/Controllers/DepartmentAcademicSessionAcademicClassPackageFeeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PackageFeeCollection;
 use App\Models\AcademicClass;
 use App\Models\AcademicClassPackageFee;
 use App\Models\AcademicSession;
@@ -28,7 +29,9 @@ class DepartmentAcademicSessionAcademicClassPackageFeeController extends Control
             ->get();
 
         return response([
-            "academic_class_package_fees" => $academic_class_package_fees ?? [],
+            "academic_class_package_fees" => $academic_class_package_fees
+                ? PackageFeeCollection::make($academic_class_package_fees)
+                : [],
         ], 200);
     }
 
@@ -59,28 +62,27 @@ class DepartmentAcademicSessionAcademicClassPackageFeeController extends Control
 
         foreach ($request->academic_class_package_fees as $item)
         {
+            // $item => {package_id: 1, fee_id: 5, amount: 100}
+
             if (in_array($item['package_id'], $active_package_ids))
             {
-                foreach ($item['fees'] as $fee)
+                if (in_array($item['fee_id'], $active_fee_ids))
                 {
-                    if (in_array($fee['fee_id'], $active_fee_ids))
-                    {
-                        $academic_class_package_fee = AcademicClassPackageFee::withTrashed()
-                            ->updateOrCreate(
-                                [
-                                    'academic_class_id' => $academic_class->id,
-                                    'package_id'        => $item['package_id'],
-                                    'fee_id'            => $fee['fee_id'],
-                                ],
-                                [
-                                    'amount'        => $fee['amount'],
-                                    'deleted_at'    => null,
-                                    'author_id'     => auth()->id() ?? null,
-                                ]
-                            );
+                    $academic_class_package_fee = AcademicClassPackageFee::withTrashed()
+                        ->updateOrCreate(
+                            [
+                                'academic_class_id' => $academic_class->id,
+                                'package_id'        => $item['package_id'],
+                                'fee_id'            => $item['fee_id'],
+                            ],
+                            [
+                                'amount'        => $item['amount'],
+                                'deleted_at'    => null,
+                                'author_id'     => auth()->id() ?? null,
+                            ]
+                        );
 
-                        $filtered_academic_class_package_fee_ids[] = $academic_class_package_fee->id;
-                    }
+                    $filtered_academic_class_package_fee_ids[] = $academic_class_package_fee->id;
                 }
             }
         }
@@ -101,7 +103,9 @@ class DepartmentAcademicSessionAcademicClassPackageFeeController extends Control
             ->get();
 
         return response([
-            "academic_class_package_fees" => $academic_class_package_fees ?? [],
+            "academic_class_package_fees" => $academic_class_package_fees 
+                ? PackageFeeCollection::make($academic_class_package_fees)
+                : [],
         ], 201);
     }
 }
---
+++ app/Http/Resources/PackageFeeResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PackageFeeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            "id"                => (int) ($this->id ?? 0),
+            "academic_class_id" => (int) ($this->academic_class_id ?? 0),
+            "package_id"        => (int) ($this->package_id ?? 0),
+            "fee_id"            => (int) ($this->fee_id ?? 0),
+            "amount"            => (int) ($this->amount ?? 0),
+        ];
+    }
+}
